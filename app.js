0547d075283a7ff7a362e4f4a37998e75f8a084c
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2024-09-12 19:23:56 -0500

    Added binary search, adding a couple of sorting algos so I can sort the list and use binsearch

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const { createArray, TreeNode, buildBST } = require('./BinarySearchTree');
+const { createArray, TreeNode, buildBST } = require("./BinarySearchTree");
+const { BinSearch } = require("./binSearch");
 
-// let a = createArray(10, -100, 100);
-let a = [18, -82, -66, -88, -83, 26, 67, -59, -63, 9];
-buildBST(a);
-// a.sort((x, y) => x - y);
+let a = createArray(10, -100, 100);
+let search = new BinSearch();
+// let a = [18, -82, -66, -88, -83, 26, 67, -59, -63, 9];
+
+console.log(search.search(a, 6));
---
+++ binSearch.js
@@ -0,0 +1,17 @@
+class BinSearch {
+  search(nums, target) {
+    let lb = 0;
+    let ub = nums.length() - 1;
+    while (lb < ub) {
+      let pointer = Math.floor((lb + ub) / 2);
+      if (nums[pointer] === target) return pointer;
+      else if (nums[pointer] < target) {
+        ub = pointer - 1;
+      } else {
+        lb = pointer + 1;
+      }
+    }
+  }
+}
+
+module.exports = BinSearch;
