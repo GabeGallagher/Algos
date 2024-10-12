3f4b7d15cc0ab58e4ba2291aa896fc79da9b8efc
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2024-10-11 19:32:08 -0500

    cleaned up the heapify code

--- binaryTree.js
+++ binaryTree.js
@@ -27,52 +27,24 @@ export default class BinaryTree {
   }
 
   heapify(treeNode) {
-    //node has 2 child nodes and
     if (treeNode.left !== null && treeNode.right !== null) {
       this.heapify(treeNode.left);
       this.heapify(treeNode.right);
-      // both child nodes are larger than parent
+      
       if (
-        treeNode.left.val > treeNode.val &&
-        treeNode.right.val > treeNode.val
-      ) {
-        if (treeNode.left.val > treeNode.right.val) {
-            this.heapSwap(treeNode.left, treeNode);
-        //   let temp = treeNode.left.val;
-        //   treeNode.left.val = treeNode.val;
-        //   treeNode.val = temp;
-        } else {
-            this.heapSwap(treeNode.right, treeNode);
-        //   let temp = treeNode.right.val;
-        //   treeNode.right.val = treeNode.val;
-        //   treeNode.val = temp;
-        }
-      }
-      // one child node is larger than parent and sibling
-      else if (
         treeNode.left.val > treeNode.val ||
         treeNode.right.val > treeNode.val
       ) {
         if (treeNode.left.val > treeNode.right.val) {
             this.heapSwap(treeNode.left, treeNode);
-        //   let temp = treeNode.left.val;
-        //   treeNode.left.val = treeNode.val;
-        //   treeNode.val = temp;
         } else {
             this.heapSwap(treeNode.right, treeNode);
-        //   let temp = treeNode.right.val;
-        //   treeNode.right.val = treeNode.val;
-        //   treeNode.val = temp;
         }
       }
     }
-    // node has 1 child node
     else if (treeNode.left !== null) {
       if (treeNode.left.val > treeNode.val) {
         this.heapSwap(treeNode.left, treeNode);
-        // let temp = treeNode.val;
-        // treeNode.val = treeNode.left.val;
-        // treeNode.left.val = temp;
       }
     }
   }
@@ -108,5 +80,5 @@ export default class BinaryTree {
   }
 }
 
-// let maxHeap = new BinaryTree().buildMaxHeap([5, 12, 64, 1, 37, 90, 91, 97]);
-// console.log(maxHeap.toString());
+let maxHeap = new BinaryTree().buildMaxHeap([5, 12, 64, 1, 37, 90, 91, 97]);
+console.log(maxHeap.toString());
